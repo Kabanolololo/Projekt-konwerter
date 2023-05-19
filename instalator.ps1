c7fbc68c1f830485fb5d9733ac6ef8adfa751348
Paweł Muszyński <p.muszynski200@gmail.com>
2023-05-19 21:34:32 +0200

    task3

--- instalator.ps1
+++ instalator.ps1
@@ -1,2 +1,3 @@
 py -m pip install pyinstaller
 py -m pip install pyyaml
+py -m pip install dicttoxml
--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import os
 import json
 import yaml
-
-def ymltojson(a):
-    with open(a) as file:
-        data = yaml.safe_load(file)
-    json_file = os.path.splitext(a)[0] + ".json"
-    with open(json_file, "w") as file:
-        json.dump(data, file, indent=4)
-    print("Plik YAML został pomyślnie przekonwertowany na JSON i zapisany jako:", json_file)
-
+import dicttoxml
 
 print("Konwerter xml, json, yml")
 a = input("Podaj nazwę pliku (Przykład: nazwa_pliku.xml): ")
@@ -27,23 +19,28 @@ if os.path.exists(a):
             print("Wybrałeś plik XML.")
         elif rozszerzenie.lower() == '.yml':
             print("Wybrałeś plik YAML.")
+# KONWERTER Z JSON DO XML ORAZ YAML JEST JUŻ GOTOWY
         elif rozszerzenie.lower() == '.json':
             try:
                 with open(a) as plik:
                     info = json.load(plik)
                 print("Składnia pliku z rozszerzeniem JSON jest poprawna.")
+                b = input("Podaj roszerzenie pliku, na który mam przekonwertować (yml , xml): ")
+                if b == 'yml':
+                    yml_file = f"{nazwa_pliku}.yml"
+                    with open(yml_file, 'w') as plik_yml:
+                        yaml.dump(info, plik_yml, default_flow_style=False)
+                    print(f"Plik JSON został przekonwertowany na YAML i zapisany jako {yml_file}.")
+                elif b == 'xml':
+                    xml_file = f"{nazwa_pliku}.xml"
+                    xml_data = dicttoxml.dicttoxml(info)
+                    with open(xml_file, 'w') as plik_xml:
+                        plik_xml.write(xml_data.decode())
+                    print(f"Plik JSON został przekonwertowany na XML i zapisany jako {xml_file}.")
+#Jeśli mamy błąd w składni jsona, wyrzuci nam w której linicje, ale kod dalej się nie wykona
             except json.JSONDecodeError as blad:
                 print("Błąd w składni pliku JSON:\n",blad)
         else:
             print("Nieobsługiwany format pliku.")
-#dodać wiecej warunków
-        b = input("Podaj rozszerzenie pliku, na który mam skonwertować (wpisz: yml , json , xml ): ")
-        if b == 'json':
-            ymltojson(a)
-        #if b == 'yml':
-        #if b == 'json':
-        else:
-            print("Nieprawidłowe rozszerzenie pliku")
-
 else:
     print("Podana ścieżka pliku nie istnieje.")
