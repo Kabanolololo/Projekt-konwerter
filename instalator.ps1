a8678313b35ec9067765897cca48e293cfde6ac5
Paweł Muszyński <p.muszynski200@gmail.com>
2023-05-20 15:23:13 +0200

    task7

--- instalator.ps1
+++ instalator.ps1
@@ -1,3 +1,4 @@
 pip install pyinstaller
 pip install pyyaml
 pip install dicttoxml
+pip install xmltodict
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import yaml
 import dicttoxml
 import xml.etree.cElementTree as ET
+import xmltodict
+
 print("Konwerter xml, json, yml")
 a = input("Podaj nazwę pliku (Przykład: nazwa_pliku.xml): ")
 a = os.path.abspath(a)
@@ -12,12 +14,28 @@ if os.path.exists(a):
         print("Podana ścieżka nie odnosi się do pliku.")
     else:
         nazwa_pliku, rozszerzenie = os.path.splitext(a)
+# KONWERTER Z XML DO YAML ORAZ JSON JEST JUŻ GOTOWY
         if rozszerzenie.lower() == '.xml':
-            print("Wybrałeś plik XML.") #usuń to potem
             try:
                 tree = ET.parse(a)
                 print("Poprawna składnia pliku XML.")
-                # Dodaj tutaj dalszą część kodu
+                b = input("Podaj rozszerzenie pliku, na które mam przekonwertować (yaml, json): ")
+                if b == 'yaml':
+                    yaml_file = f"{nazwa_pliku}.yaml"
+                    xml_root = tree.getroot()
+                    xml_dict = xmltodict.parse(ET.tostring(xml_root))
+                    with open(yaml_file, 'w') as plik_yaml:
+                        yaml.dump(xml_dict, plik_yaml)
+                    print(f"Plik XML został przekonwertowany na YAML i zapisany jako {yaml_file}.")
+                elif b == 'json':
+                    json_file = f"{nazwa_pliku}.json"
+                    xml_root = tree.getroot()
+                    xml_dict = xmltodict.parse(ET.tostring(xml_root))
+                    with open(json_file, 'w') as plik_json:
+                        json.dump(xml_dict, plik_json)
+                    print(f"Plik XML został przekonwertowany na JSON i zapisany jako {json_file}.")
+                else:
+                    print("Niepoprawne rozszerzenie pliku.")
             except ET.ParseError as error:
                 print("Błąd składni pliku XML:\n", error)
 # KONWERTER Z YML DO XML ORAZ JSON JEST JUŻ GOTOWY
@@ -38,6 +56,8 @@ if os.path.exists(a):
                     with open(xml_file, 'w') as plik_xml:
                         plik_xml.write(xml_data.decode())
                     print(f"Plik YAML został przekonwertowany na XML i zapisany jako {xml_file}.")
+                else:
+                    print("Niepoprawne rozszerzenie pliku.")
             except yaml.YAMLError as blad:
                 print("Błąd w składni pliku YAML:\n", blad)
 # KONWERTER Z JSON DO XML ORAZ YAML JEST JUŻ GOTOWY
@@ -58,6 +78,8 @@ if os.path.exists(a):
                     with open(xml_file, 'w') as plik_xml:
                         plik_xml.write(xml_data.decode())
                     print(f"Plik JSON został przekonwertowany na XML i zapisany jako {xml_file}.")
+                else:
+                    print("Niepoprawne rozszerzenie pliku.")
             except json.JSONDecodeError as blad:
                 print("Błąd w składni pliku JSON:\n",blad)
         else:
